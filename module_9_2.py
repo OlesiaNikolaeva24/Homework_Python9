d1f61cb4db97a19e9aa78e8f6f713ed374941536
OlesiaNikolaeva24 <kobaltbrom@gmail.com>
2024-10-11 00:15:24 +0900

    11/10

---
+++ module_9_2.py
@@ -0,0 +1,16 @@
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+first_second = first_strings + second_strings
+
+first_result = [len(x) for x in first_strings if len(x) >= 5]
+second_result = [(x, y) for x in first_strings for y in second_strings if len(x) == len(y)]
+third_result = {x: len(x) for x in first_second if len(x) % 2 == 0}
+
+
+print(first_result)
+print(second_result)
+print(third_result)
+
+# [10, 8, 8]
+# [('Elon', 'Task'), ('Elon', 'Java'), ('Musk', 'Task'), ('Musk', 'Java'), ('Monitors', 'Computer'), ('Variable', 'Computer')]
+# {'Elon': 4, 'Musk': 4, 'Programmer': 10, 'Monitors': 8, 'Variable': 8, 'Task': 4, 'Java': 4, 'Computer': 8}
